81d7437c185e34c8fca525aaeef051bfdd79d1cd
Jyoti-yadav09 <raniyadav09110@gmail.com>
2025-03-12 11:36:03 +0545

    Updating with new content

--- Withusecontext/src/App.jsx
+++ Withusecontext/src/App.jsx
@@ -1,33 +1,30 @@
-import {useState,createContext, useContext} from 'react';
+import {useState, useContext, createContext} from 'react';
 
-  const Messagecontext=createContext();
+const MessageContext=createContext(); 
 const App = () => {
-
-   const[message, setMessage]=useState("Hello, Bestie");
+  const[message, setMessage]=useState("Hello, Bestie");
   return (
-    
-      <Messagecontext.Provider value={message}>
-        <Parent/>
-      </Messagecontext.Provider>
-  
+    <MessageContext.Provider value={message}>
+      <Parent/>
+    </MessageContext.Provider>
   );
 };
-       const Parent=()=>
-       {
-         return <Child/>
-       };
+  
+ const Parent=()=>
+ {
+   return <Child/>;
+ };
+ 
+   const Child=()=>
+   {
+     return <Grandchild/>;
+   };
 
-       const Child=()=>
-       {
-         return <GrandChild/>
-       };
-
-
-         const GrandChild=()=>
-         {
-          const message=useContext(Messagecontext);
-          return <p>{message}</p>
-         };
+    const Grandchild=()=>
+    {
+      const message=useContext(MessageContext); 
+       return <h1>{message}</h1>;
+    };
 
 
 export default App;
