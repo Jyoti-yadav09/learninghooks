6afd92eb5c2165323a5722b07b973b5626abc74f
Jyoti-yadav09 <raniyadav09110@gmail.com>
2025-03-17 12:37:21 +0545

    Adding the CSS using tailwind css

--- Usecontextexmpl/src/components/Dashboard.jsx
+++ Usecontextexmpl/src/components/Dashboard.jsx
@@ -4,8 +4,8 @@ const Dashboard = () => {
     const {isLoggedIn}=useContext(AuthContext);
   return (
     <div>
-      <h2>Dashboard</h2>
-      {isLoggedIn? <p>Welcome, User!</p>: <p>Please login first</p>}
+      <h2 className='text-xl text-teal-600'>Dashboard</h2>
+      {isLoggedIn? <p className='text-xl'>Welcome, User!</p>: <p className='text-xl text-red-600'>Please login first</p>}
     </div>
   );
 };
--- Usecontextexmpl/src/components/Navbar.jsx
+++ Usecontextexmpl/src/components/Navbar.jsx
@@ -5,12 +5,12 @@ import  { AuthContext } from './AuthProvider';
 const Navbar = () => {
     const {isLoggedIn, login, logout}= useContext(AuthContext);
   return (
-    <nav className="bg-red-400 border-amber-300 border-double">
-      <h1>My App</h1>
+    <nav className="bg-red-400 border-amber-300 border-dotted flex content-center">
+      <h1 className='text-2xl pr-10 text-white'>My App</h1>
       {isLoggedIn ? (
-        <button onClick={logout}>Logout</button>
+        <button onClick={logout} className='text-xl text-white hover:text-purple-950'>Logout</button>
       ):(
-        <button onClick={login}>Login</button>
+        <button onClick={login} className='text-xl text-white hover:text-purple-950'>Login</button>
       )}
     </nav>
   );
