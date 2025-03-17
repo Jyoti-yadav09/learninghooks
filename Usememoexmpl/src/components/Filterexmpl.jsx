b40516bfd3415edb959f79414b9bbbbed1395298
Jyoti-yadav09 <raniyadav09110@gmail.com>
2025-03-17 14:18:38 +0545

    Adding file for learning filtering numbers using usememo

--- Usememoexmpl/src/components/Filterexmpl.jsx
+++ Usememoexmpl/src/components/Filterexmpl.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import {usestate, useMemo, useState} from 'react';
 
 const Filterexmpl = () => {
+    const [numbers]=useState([1,2,3,4,5,6,7,8,9,10]);
+    const[filterEven, setFilterEven]= useState(true);
+
+    const filteredNumbers=useMemo(()=>
+    {
+         console.log("Filtering numbers");
+          return numbers.filter(number=>(filterEven ? number %2===0: number %2 !==0));
+
+    }, [numbers, filterEven]);
   return (
     <div>
-      
+      <h1>Numbers: {filteredNumbers.join(",")}</h1>
+      <button onClick={()=>setFilterEven(!filterEven)}>
+        Toggle Filter: {filterEven ? "Even" :"Odd"}
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Filterexmpl
+export default Filterexmpl;
